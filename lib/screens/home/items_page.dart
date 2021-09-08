05d6f7b4d71a2233bf087bb4dc7fa6c863f2f043
Victorer <victor@myicellar.com>
2021-09-08 15:07:32 +0900

    71-shimmer(loading with layout preview).cmproj

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -2,6 +2,7 @@ import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tomato_record/constants/common_size.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ItemsPage extends StatelessWidget {
   const ItemsPage({Key? key}) : super(key: key);
@@ -13,89 +14,195 @@ class ItemsPage extends StatelessWidget {
         Size size = MediaQuery.of(context).size;
         final imgSize = size.width / 4;
 
-        return ListView.separated(
-          padding: EdgeInsets.all(common_padding),
-          separatorBuilder: (context, index) {
-            return Divider(
-              height: common_padding * 2 + 1,
-              thickness: 1,
-              color: Colors.grey[200],
-              indent: common_sm_padding,
-              endIndent: common_sm_padding,
-            );
-          },
-          itemBuilder: (context, index) {
-            return SizedBox(
-              height: imgSize,
-              child: Row(
-                children: [
-                  SizedBox(
-                      height: imgSize,
-                      width: imgSize,
-                      child: ExtendedImage.network(
-                        'https://picsum.photos/100',
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(12),
-                      )),
-                  SizedBox(
-                    width: common_sm_padding,
-                  ),
-                  Expanded(
-                      child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'work',
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
-                      Text(
-                        '53일전',
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                      Text('5000원'),
-                      Expanded(
-                        child: Container(),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            height: 14,
-                            child: FittedBox(
-                              fit: BoxFit.fitHeight,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    CupertinoIcons.chat_bubble_2,
-                                    color: Colors.grey,
-                                  ),
-                                  Text(
-                                    '23',
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                  Icon(
-                                    CupertinoIcons.heart,
-                                    color: Colors.grey,
-                                  ),
-                                  Text(
-                                    '30',
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ))
-                ],
-              ),
-            );
-          },
-          itemCount: 10,
+        return FutureBuilder(
+            future: Future.delayed(Duration(seconds: 2)),
+            builder: (context, snapshot) {
+              return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                  child: (snapshot.connectionState != ConnectionState.done)
+                      ? _shimmerListView(imgSize)
+                      : _listView(imgSize));
+            });
+        // return _listView(imgSize);
+      },
+    );
+  }
+
+  ListView _listView(double imgSize) {
+    return ListView.separated(
+      padding: EdgeInsets.all(common_padding),
+      separatorBuilder: (context, index) {
+        return Divider(
+          height: common_padding * 2 + 1,
+          thickness: 1,
+          color: Colors.grey[200],
+          indent: common_sm_padding,
+          endIndent: common_sm_padding,
         );
       },
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: imgSize,
+          child: Row(
+            children: [
+              SizedBox(
+                  height: imgSize,
+                  width: imgSize,
+                  child: ExtendedImage.network(
+                    'https://picsum.photos/100',
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+              SizedBox(
+                width: common_sm_padding,
+              ),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'work',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  Text(
+                    '53일전',
+                    style: Theme.of(context).textTheme.subtitle2,
+                  ),
+                  Text('5000원'),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 14,
+                        child: FittedBox(
+                          fit: BoxFit.fitHeight,
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.chat_bubble_2,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                '23',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              Icon(
+                                CupertinoIcons.heart,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                '30',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ))
+            ],
+          ),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
+  Widget _shimmerListView(double imgSize) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      enabled: true,
+      child: ListView.separated(
+        padding: EdgeInsets.all(common_padding),
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: common_padding * 2 + 1,
+            thickness: 1,
+            color: Colors.grey[200],
+            indent: common_sm_padding,
+            endIndent: common_sm_padding,
+          );
+        },
+        itemBuilder: (context, index) {
+          return SizedBox(
+            height: imgSize,
+            child: Row(
+              children: [
+                Container(
+                    height: imgSize,
+                    width: imgSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                SizedBox(
+                  width: common_sm_padding,
+                ),
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        height: 14,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3),
+                        )),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Container(
+                        height: 12,
+                        width: 180,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3),
+                        )),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Container(
+                        height: 14,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3),
+                        )),
+                    Expanded(
+                      child: Container(),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                            height: 14,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(3),
+                            )),
+                      ],
+                    )
+                  ],
+                ))
+              ],
+            ),
+          );
+        },
+        itemCount: 10,
+      ),
     );
   }
 }

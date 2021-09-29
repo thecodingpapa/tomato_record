78c46b120e602e223c61c2eed9af79625f478d2e
Victorer <victor@myicellar.com>
2021-09-29 11:21:13 +0900

    88-pick images from gallery.cmproj

--- lib/screens/input/multi_image_select.dart
+++ lib/screens/input/multi_image_select.dart
@@ -1,11 +1,21 @@
+import 'dart:typed_data';
+
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tomato_record/constants/common_size.dart';
 
-class MultiImageSelect extends StatelessWidget {
-  const MultiImageSelect({
+class MultiImageSelect extends StatefulWidget {
+  MultiImageSelect({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<MultiImageSelect> createState() => _MultiImageSelectState();
+}
+
+class _MultiImageSelectState extends State<MultiImageSelect> {
+  List<XFile> _images = [];
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +32,39 @@ class MultiImageSelect extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.all(common_padding),
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.camera_alt_rounded,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        '0/10',
-                        style: Theme.of(context).textTheme.subtitle2,
-                      )
-                    ],
+                child: InkWell(
+                  onTap: () async {
+                    final ImagePicker _picker = ImagePicker();
+                    final List<XFile>? images = await _picker.pickMultiImage();
+                    if (images != null && images.isNotEmpty) {
+                      _images.clear();
+                      _images.addAll(images);
+                      setState(() {});
+                    }
+                  },
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.camera_alt_rounded,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          '0/10',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        )
+                      ],
+                    ),
+                    width: imageSize,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(imageCorner),
+                        border: Border.all(color: Colors.grey, width: 1)),
                   ),
-                  width: imageSize,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(imageCorner),
-                      border: Border.all(color: Colors.grey, width: 1)),
                 ),
               ),
               ...List.generate(
-                  100,
+                  _images.length,
                   (index) => Stack(
                         children: [
                           Padding(
@@ -51,13 +72,42 @@ class MultiImageSelect extends StatelessWidget {
                                 right: common_padding,
                                 top: common_padding,
                                 bottom: common_padding),
-                            child: ExtendedImage.network(
-                              'https://picsum.photos/100',
-                              width: imageSize,
-                              height: imageSize,
-                              borderRadius: BorderRadius.circular(imageCorner),
-                              shape: BoxShape.rectangle,
-                            ),
+                            child: FutureBuilder<Uint8List>(
+                                future: _images[index].readAsBytes(),
+                                builder: (context, snapshot) {
+                                  if (snapshot.hasData) {
+                                    return ExtendedImage.memory(
+                                      snapshot.data!,
+                                      width: imageSize,
+                                      height: imageSize,
+                                      fit: BoxFit.cover,
+                                      loadStateChanged: (state) {
+                                        switch (state.extendedImageLoadState) {
+                                          case LoadState.loading:
+                                            return Container(
+                                                width: imageSize,
+                                                height: imageSize,
+                                                padding: EdgeInsets.all(
+                                                    imageSize / 3),
+                                                child:
+                                                    CircularProgressIndicator());
+                                          case LoadState.completed:
+                                            return null;
+                                          case LoadState.failed:
+                                            return Icon(Icons.cancel);
+                                        }
+                                      },
+                                      borderRadius:
+                                          BorderRadius.circular(imageCorner),
+                                      shape: BoxShape.rectangle,
+                                    );
+                                  } else {
+                                    return Container(
+                                        width: imageSize,
+                                        height: imageSize,
+                                        child: CircularProgressIndicator());
+                                  }
+                                }),
                           ),
                           Positioned(
                             right: 0,

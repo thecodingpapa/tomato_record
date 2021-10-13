b19c96afcd762ffa1ef64aa01486f1265beb917e
Victorer <victor@myicellar.com>
2021-10-13 15:40:32 +0900

    105-pageview with indicator.cmproj

--- lib/screens/item/item_detail_screen.dart
+++ lib/screens/item/item_detail_screen.dart
@@ -1,4 +1,9 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:tomato_record/constants/common_size.dart';
+import 'package:tomato_record/data/item_model.dart';
+import 'package:tomato_record/repo/item_service.dart';
 
 class ItemDetailScreen extends StatefulWidget {
   final String itemKey;
@@ -9,13 +14,94 @@ class ItemDetailScreen extends StatefulWidget {
 }
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
+  PageController _pageController = PageController();
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.cyan,
-        child: Center(child: Text('item key is ${widget.itemKey}')),
-      ),
-    );
+    return FutureBuilder<ItemModel>(
+        future: ItemService().getItem(widget.itemKey),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            ItemModel itemModel = snapshot.data!;
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                Size _size = MediaQuery.of(context).size;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Scaffold(
+                      body: CustomScrollView(
+                        slivers: [
+                          SliverAppBar(
+                            expandedHeight: _size.width,
+                            pinned: true,
+                            flexibleSpace: Stack(
+                              children: [
+                                FlexibleSpaceBar(
+                                  background: PageView.builder(
+                                    controller: _pageController,
+                                    allowImplicitScrolling: true,
+                                    itemBuilder: (context, index) {
+                                      return ExtendedImage.network(
+                                        itemModel.imageDownloadUrls[index],
+                                        fit: BoxFit.cover,
+                                        scale: 0.1,
+                                      );
+                                    },
+                                    itemCount:
+                                        itemModel.imageDownloadUrls.length,
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 0,
+                                  right: 0,
+                                  bottom: common_sm_padding,
+                                  child: Container(
+                                    child: Center(
+                                      child: SmoothPageIndicator(
+                                          controller:
+                                              _pageController, // PageController
+                                          count: itemModel
+                                              .imageDownloadUrls.length,
+                                          effect: WormEffect(
+                                              activeDotColor: Theme.of(context)
+                                                  .primaryColor,
+                                              dotColor: Theme.of(context)
+                                                  .colorScheme
+                                                  .background,
+                                              radius: 4,
+                                              dotHeight: 8,
+                                              dotWidth:
+                                                  8), // yo// ur preferred effect
+                                          onDotClicked: (index) {}),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SliverToBoxAdapter(
+                            child: Container(
+                              height: _size.height * 2,
+                              color: Colors.cyan,
+                              child: Center(
+                                  child: Text('item key is ${widget.itemKey}')),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+          return Container();
+        });
   }
 }

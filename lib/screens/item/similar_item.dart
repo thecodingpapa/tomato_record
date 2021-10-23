22e741b9c5ed53cd44aa230dd069e2c08fb9043f
Victorer <victor@myicellar.com>
2021-10-23 09:51:37 +0900

    119-how to stack the item detail pages.cmproj

--- lib/screens/item/similar_item.dart
+++ lib/screens/item/similar_item.dart
@@ -1,7 +1,9 @@
+import 'package:beamer/src/beamer.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:tomato_record/constants/common_size.dart';
 import 'package:tomato_record/data/item_model.dart';
+import 'package:tomato_record/screens/item/item_detail_screen.dart';
 
 class SimilarItem extends StatelessWidget {
   final ItemModel _itemModel;
@@ -9,33 +11,40 @@ class SimilarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        AspectRatio(
-          aspectRatio: 5 / 4,
-          child: ExtendedImage.network(
-            _itemModel.imageDownloadUrls[0],
-            fit: BoxFit.cover,
-            borderRadius: BorderRadius.circular(8),
-            shape: BoxShape.rectangle,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return ItemDetailScreen(_itemModel.itemKey);
+        }));
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AspectRatio(
+            aspectRatio: 5 / 4,
+            child: ExtendedImage.network(
+              _itemModel.imageDownloadUrls[0],
+              fit: BoxFit.cover,
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
           ),
-        ),
-        Text(
-          _itemModel.title,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: common_sm_padding),
-          child: Text(
-            '${_itemModel.price.toString()}원',
-            style: Theme.of(context).textTheme.subtitle2,
+          Text(
+            _itemModel.title,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: Theme.of(context).textTheme.subtitle1,
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: common_sm_padding),
+            child: Text(
+              '${_itemModel.price.toString()}원',
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+          )
+        ],
+      ),
     );
   }
 }

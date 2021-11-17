1d766d33b65ec1b9d0236c11e2dd707c341bf95a
Victorer <victor@myicellar.com>
2021-11-17 15:20:01 +0900

    127.1 - fix authentication error

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -68,6 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
           IconButton(
               onPressed: () {
                 FirebaseAuth.instance.signOut();
+                context.beamToNamed("/");
               },
               icon: Icon(CupertinoIcons.nosign)),
           IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),

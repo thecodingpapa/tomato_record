3baedf62328b3a3b52a214b3d7b2ea7d16ee91d7
Victorer <victor@myicellar.com>
2021-11-21 12:43:41 +0900

    137-chat layout

---
+++ lib/screens/chat/chat.dart
@@ -0,0 +1,87 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+const roundedCorner = Radius.circular(20);
+
+class Chat extends StatelessWidget {
+  final Size size;
+  final bool isMine;
+  const Chat({Key? key, required this.size, required this.isMine})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return isMine ? _buildMyMsg() : _buildOthersMsg();
+  }
+
+  Row _buildOthersMsg() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ExtendedImage.network(
+          'https://randomuser.me/api/portraits/women/26.jpg',
+          width: 40,
+          height: 40,
+          fit: BoxFit.cover,
+          borderRadius: BorderRadius.circular(6),
+          shape: BoxShape.rectangle,
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Text(
+                  'l;kjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjoi'),
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              constraints:
+                  BoxConstraints(minHeight: 40, maxWidth: size.width * 0.5),
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.only(
+                      topRight: roundedCorner,
+                      topLeft: Radius.circular(2),
+                      bottomRight: roundedCorner,
+                      bottomLeft: roundedCorner)),
+            ),
+            SizedBox(
+              width: 6,
+            ),
+            Text('오전 10:25'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Row _buildMyMsg() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text('오전 10:25'),
+        SizedBox(
+          width: 6,
+        ),
+        Container(
+          child: Text(
+              'l;kjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjokjaklfjd;lajsdkfa;kdfjoi'),
+          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          constraints:
+              BoxConstraints(minHeight: 40, maxWidth: size.width * 0.6),
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                  topLeft: roundedCorner,
+                  topRight: Radius.circular(2),
+                  bottomRight: roundedCorner,
+                  bottomLeft: roundedCorner)),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/chat/chatroom_screen.dart
+++ lib/screens/chat/chatroom_screen.dart
@@ -1,5 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:tomato_record/screens/chat/chat.dart';
 
 class ChatroomScreen extends StatefulWidget {
   final String chatroomKey;
@@ -13,86 +14,109 @@ class ChatroomScreen extends StatefulWidget {
 class _ChatroomScreenState extends State<ChatroomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: Colors.grey[200],
-      body: SafeArea(
-        child: Column(
-          children: [
-            MaterialBanner(
-              padding: EdgeInsets.zero,
-              leadingPadding: EdgeInsets.zero,
-              actions: [Container()],
-              content: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    dense: true,
-                    minVerticalPadding: 0,
-                    leading: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 12.0, top: 8, bottom: 4),
-                      child: ExtendedImage.network(
-                        'https://randomuser.me/api/portraits/lego/8.jpg',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    contentPadding: EdgeInsets.only(left: 4, right: 0),
-                    title: RichText(
-                      text: TextSpan(
-                          text: '거래완료',
-                          style: Theme.of(context).textTheme.bodyText1,
-                          children: [
-                            TextSpan(
-                                text: ' 이케아 소르테라 분리수거함 5개',
-                                style: Theme.of(context).textTheme.bodyText2)
-                          ]),
-                    ),
-                    subtitle: RichText(
-                      text: TextSpan(
-                          text: '30000원',
-                          style: Theme.of(context).textTheme.bodyText1,
-                          children: [
-                            TextSpan(
-                                text: '(가격제안불가)',
-                                style: Theme.of(context).textTheme.bodyText2!
-                                  ..copyWith(color: Colors.black12))
-                          ]),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, bottom: 12),
-                    child: SizedBox(
-                      height: 32,
-                      child: TextButton.icon(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.edit,
-                            size: 16,
-                            color: Colors.black87,
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        Size _size = MediaQuery.of(context).size;
+        return Scaffold(
+          appBar: AppBar(),
+          backgroundColor: Colors.grey[200],
+          body: SafeArea(
+            child: Column(
+              children: [
+                MaterialBanner(
+                  padding: EdgeInsets.zero,
+                  leadingPadding: EdgeInsets.zero,
+                  actions: [Container()],
+                  content: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        dense: true,
+                        minVerticalPadding: 0,
+                        leading: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 12.0, top: 8, bottom: 4),
+                          child: ExtendedImage.network(
+                            'https://randomuser.me/api/portraits/lego/8.jpg',
+                            fit: BoxFit.cover,
                           ),
-                          label: Text('후기 남기기',
-                              style: Theme.of(context).textTheme.bodyText1!
-                                ..copyWith(color: Colors.black87)),
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(4),
-                                  side: BorderSide(
-                                      color: Colors.grey[300]!, width: 1)))),
-                    ),
+                        ),
+                        contentPadding: EdgeInsets.only(left: 4, right: 0),
+                        title: RichText(
+                          text: TextSpan(
+                              text: '거래완료',
+                              style: Theme.of(context).textTheme.bodyText1,
+                              children: [
+                                TextSpan(
+                                    text: ' 이케아 소르테라 분리수거함 5개',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2)
+                              ]),
+                        ),
+                        subtitle: RichText(
+                          text: TextSpan(
+                              text: '30000원',
+                              style: Theme.of(context).textTheme.bodyText1,
+                              children: [
+                                TextSpan(
+                                    text: '(가격제안불가)',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2!
+                                          ..copyWith(color: Colors.black12))
+                              ]),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16, bottom: 12),
+                        child: SizedBox(
+                          height: 32,
+                          child: TextButton.icon(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.edit,
+                                size: 16,
+                                color: Colors.black87,
+                              ),
+                              label: Text('후기 남기기',
+                                  style: Theme.of(context).textTheme.bodyText1!
+                                    ..copyWith(color: Colors.black87)),
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4),
+                                      side: BorderSide(
+                                          color: Colors.grey[300]!,
+                                          width: 1)))),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                    child: Container(
+                  color: Colors.white,
+                  child: ListView.separated(
+                      padding: EdgeInsets.all(16),
+                      itemBuilder: (context, index) {
+                        bool isMine = (index % 2) == 0;
+                        return Chat(
+                          size: _size,
+                          isMine: isMine,
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 12,
+                        );
+                      },
+                      itemCount: 30),
+                )),
+                _buildInputBar()
+              ],
             ),
-            Expanded(
-                child: Container(
-              color: Colors.yellowAccent,
-            )),
-            _buildInputBar()
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 

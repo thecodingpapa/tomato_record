def5bb6d48eaae884b104056b851db490ca41c2c
Victorer <victor@myicellar.com>
2021-11-25 11:15:14 +0900

    145-refresh item list (RefreshIndicator).cmproj

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -8,8 +8,25 @@ import 'package:tomato_record/repo/item_service.dart';
 import 'package:beamer/beamer.dart';
 import 'package:tomato_record/router/locations.dart';
 
-class ItemsPage extends StatelessWidget {
+class ItemsPage extends StatefulWidget {
   const ItemsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ItemsPage> createState() => _ItemsPageState();
+}
+
+class _ItemsPageState extends State<ItemsPage> {
+  bool init = false;
+  List<ItemModel> items = [];
+
+  @override
+  void initState() {
+    if (!init) {
+      _onRefresh();
+      init = !init;
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,109 +35,113 @@ class ItemsPage extends StatelessWidget {
         Size size = MediaQuery.of(context).size;
         final imgSize = size.width / 4;
 
-        return FutureBuilder<List<ItemModel>>(
-            future: ItemService().getItems(),
-            builder: (context, snapshot) {
-              return AnimatedSwitcher(
-                  duration: Duration(milliseconds: 300),
-                  child: (snapshot.hasData && snapshot.data!.isNotEmpty)
-                      ? _listView(imgSize, snapshot.data!)
-                      : _shimmerListView(imgSize));
-            });
-        // return _listView(imgSize);
+        return AnimatedSwitcher(
+            duration: Duration(milliseconds: 300),
+            child: (items.isNotEmpty)
+                ? _listView(imgSize)
+                : _shimmerListView(imgSize));
       },
     );
   }
 
-  ListView _listView(double imgSize, List<ItemModel> items) {
-    return ListView.separated(
-      padding: EdgeInsets.all(common_padding),
-      separatorBuilder: (context, index) {
-        return Divider(
-          height: common_padding * 2 + 1,
-          thickness: 1,
-          color: Colors.grey[200],
-          indent: common_sm_padding,
-          endIndent: common_sm_padding,
-        );
-      },
-      itemBuilder: (context, index) {
-        ItemModel item = items[index];
-        return InkWell(
-          onTap: () {
-            context.beamToNamed('/$LOCATION_ITEM/${item.itemKey}');
-          },
-          child: SizedBox(
-            height: imgSize,
-            child: Row(
-              children: [
-                SizedBox(
-                    height: imgSize,
-                    width: imgSize,
-                    child: ExtendedImage.network(
-                      item.imageDownloadUrls[0],
-                      fit: BoxFit.cover,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.circular(12),
-                    )),
-                SizedBox(
-                  width: common_sm_padding,
-                ),
-                Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item.title,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                    Text(
-                      '53일전',
-                      style: Theme.of(context).textTheme.subtitle2,
-                    ),
-                    Text('${item.price.toString()}원'),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          height: 14,
-                          child: FittedBox(
-                            fit: BoxFit.fitHeight,
-                            child: Row(
-                              children: [
-                                Icon(
-                                  CupertinoIcons.chat_bubble_2,
-                                  color: Colors.grey,
-                                ),
-                                Text(
-                                  '23',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                                Icon(
-                                  CupertinoIcons.heart,
-                                  color: Colors.grey,
-                                ),
-                                Text(
-                                  '30',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ],
+  Future _onRefresh() async {
+    items.clear();
+    items.addAll(await ItemService().getItems());
+    setState(() {});
+  }
+
+  Widget _listView(double imgSize) {
+    return RefreshIndicator(
+      onRefresh: _onRefresh,
+      child: ListView.separated(
+        padding: EdgeInsets.all(common_padding),
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: common_padding * 2 + 1,
+            thickness: 1,
+            color: Colors.grey[200],
+            indent: common_sm_padding,
+            endIndent: common_sm_padding,
+          );
+        },
+        itemBuilder: (context, index) {
+          ItemModel item = items[index];
+          return InkWell(
+            onTap: () {
+              context.beamToNamed('/$LOCATION_ITEM/${item.itemKey}');
+            },
+            child: SizedBox(
+              height: imgSize,
+              child: Row(
+                children: [
+                  SizedBox(
+                      height: imgSize,
+                      width: imgSize,
+                      child: ExtendedImage.network(
+                        item.imageDownloadUrls[0],
+                        fit: BoxFit.cover,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(12),
+                      )),
+                  SizedBox(
+                    width: common_sm_padding,
+                  ),
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item.title,
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text(
+                        '53일전',
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                      Text('${item.price.toString()}원'),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 14,
+                            child: FittedBox(
+                              fit: BoxFit.fitHeight,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.chat_bubble_2,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                    '23',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  Icon(
+                                    CupertinoIcons.heart,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                    '30',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                ))
-              ],
+                        ],
+                      )
+                    ],
+                  ))
+                ],
+              ),
             ),
-          ),
-        );
-      },
-      itemCount: items.length,
+          );
+        },
+        itemCount: items.length,
+      ),
     );
   }
 
